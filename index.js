b853d83fb6f55e621b5448b3ab72582e5f521081
sandermaasikamae <147600257+sandermaasikamae@users.noreply.github.com>
2023-10-11 08:58:47 +0000

    added /api/date function

--- index.js
+++ index.js
@@ -20,8 +20,14 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date)
+  if (date.toString() !== 'Invalid Date'){
+      res.json({"unix": Date.parse(date), "utc": date.toUTCString()});
+  }
+  else {
+      res.json({"error": date.toString()});
+  }
 });
 
 
